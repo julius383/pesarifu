8e7005edb56589eec8c0f09a73a422f4664e038d
brayo333 <mulaabrian@gmail.com>
2023-08-15 09:58:41 +0300

    feat: added new homepage sections

--- website/src/views/HomePage/index.tsx
+++ website/src/views/HomePage/index.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import HeroSection from "./Sections/HeroSection";
 import IntroSection from "./Sections/IntroSection";
+import BenefitsSection from "./Sections/BenefitsSection";
+import WhyChooseUsSection from "./Sections/WhyChooseUsSection";
 
 const HomePage: React.FC<{}> = () => {
   return (
     <div className="w-full flex flex-col">
       <HeroSection />
       <IntroSection />
+      <BenefitsSection />
+      <WhyChooseUsSection />
     </div>
   );
 };
