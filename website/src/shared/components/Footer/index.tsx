d1753783b0446f96e5dbb593dcb43d5a7c6e7602
brayo333 <mulaabrian@gmail.com>
2023-09-12 00:59:59 +0300

    refactor: re-did footer

--- website/src/shared/components/Footer/index.tsx
+++ website/src/shared/components/Footer/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import {
-  ISocialMediaProps,
-  social_media_data,
-} from "../../../data/social-media";
-import { Icon } from "@iconify/react";
-import Button from "../Button";
+import "./footer.css";
+// import {
+//   ISocialMediaProps,
+//   social_media_data,
+// } from "../../../data/social-media";
+// import { Icon } from "@iconify/react";
+// import Button from "../Button";
+import { Link } from "react-router-dom";
 
 const Footer: React.FC<{}> = () => {
   function getYear() {
@@ -17,11 +19,11 @@ const Footer: React.FC<{}> = () => {
 
   return (
     <footer className="w-full h-full flex flex-row justify-center appBgColorBlack">
-      <div className="max-w-[1536px] w-full h-full md:px-7 md-min:px-20 py-[40px] flex flex-col gap-3 justify-between textColorLight">
+      <div className="max-w-[1536px] w-full h-full md:px-7 md-min:px-20 py-[40px] flex flex-col gap-3 justify-between textColorLight relative">
         <div className="w-full flex flex-col justify-between">
           <div></div>
 
-          <div className="flex flex-row items-center z-10">
+          {/* <div className="flex flex-row items-center z-10">
             {social_media_data.map((item: ISocialMediaProps, index: number) => (
               <Button
                 key={index}
@@ -42,6 +44,17 @@ const Footer: React.FC<{}> = () => {
                 onClick={() => openUrl(item.link)}
               />
             ))}
+          </div> */}
+
+          <div className="absolute top-[50%] left-[50%] centerAbsolute">
+            <Link to="/" className="text-xl font-medium textColorLight">
+              {/* <img
+              src={isDarkMode ? Logo : LogoDark}
+              alt="Logo"
+              className="h-7 pointer-events-none"
+            /> */}
+              Logo
+            </Link>
           </div>
         </div>
 
@@ -49,8 +62,7 @@ const Footer: React.FC<{}> = () => {
         <div className="w-full flex flex-row justify-between items-center">
           <div className="">
             <p className="text-sm">
-              &copy; {getYear()}{" "}
-              <a href="https://google.com/"> Pesarifu.</a>
+              &copy; {getYear()} <a href="https://google.com/"> Pesarifu.</a>
             </p>
           </div>
 
