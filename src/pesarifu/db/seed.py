a8072960d1541330e7ecd6805644ce614d01af2c
Julius Kibunjia <kibunjiajulius@gmail.com>
2024-01-25 23:27:56 +0300

    feat: add functions to seed db to aid testing

---
+++ src/pesarifu/db/seed.py
@@ -0,0 +1,188 @@
+import random
+import sys
+from functools import partial
+from math import ceil
+from operator import methodcaller
+
+from faker import Faker
+from icecream import ic
+from sqlalchemy.orm import Session
+from toolz import compose, concat
+
+from pesarifu.db.models import (
+    Base,
+    MobileMoneyAccount,
+    OrgType,
+    Provider,
+    ProviderAccount,
+    SafaricomBuygoodsAccount,
+    SafaricomPaybillAccount,
+    Transaction,
+    TransactionalAccount,
+    UserAccount,
+    WebReport,
+)
+
+
+def create_tables(engine):
+    Base.metadata.drop_all(engine)
+    all_columns = []
+    for i in [
+        UserAccount,
+        Provider,
+        TransactionalAccount,
+        WebReport,
+        SafaricomBuygoodsAccount,
+        SafaricomPaybillAccount,
+        ProviderAccount,
+        Transaction,
+        MobileMoneyAccount,
+    ]:
+        all_columns.extend(i.__table__.columns)
+    return
+
+
+def fake_provider(fake: Faker, static=True):
+    if static:
+        return {
+            "name": "Safaricom",
+            "organization_type": OrgType.TELCO,
+        }
+    else:
+        return {
+            "name": fake.unique.company,
+            "organization_type": partial(
+                random.choice, list(OrgType.__members__.values())
+            ),
+        }
+
+
+def fake_transactionalaccount(fake: Faker):
+    return {"account_name": fake.unique.company}
+
+
+def fake_mobilemoneyaccount(fake: Faker):
+    return {
+        "account_name": fake.unique.company,
+        "maybe_number": partial(fake.numerify, "+254 7########"),
+        "phone_number": "maybe_number",
+    }
+
+
+def fake_safaricompaybillaccount(fake: Faker):
+    return {
+        "account_name": fake.unique.company,
+        "paybill_number": partial(fake.numerify, "#######"),
+        "account_number": partial(fake.lexify, "?" * 10),
+    }
+
+
+def fake_safaricombuygoodsaccount(fake: Faker):
+    return {
+        "account_name": fake.unique.company,
+        "buygoods_number": partial(fake.numerify, "#######"),
+    }
+
+
+def fake_useraccount(fake: Faker, email=None, phone_number=None):
+    return {
+        "email": fake.unique.email if email is None else email,
+        "username": fake.name,
+        "phone_number": partial(fake.numerify, "+254 #########")
+        if phone_number is None
+        else phone_number,
+    }
+
+
+def random_weighted_range(ranges, weights):
+    fns = [partial(random.randrange, start, stop) for (start, stop) in ranges]
+    return random.choices(fns, weights=weights).pop()
+
+
+def fake_transaction(fake: Faker, credit_ratio=0.55):
+    # ranges and weights referenced from example safaricom transaction statement
+    ranges = [
+        (0, 500),
+        (501, 1000),
+        (1001, 2500),
+        (2501, 5000),
+        (5001, 7500),
+        (7501, 10000),
+        (10000, 50000),
+    ]
+    weights = [41, 12, 35, 6, 5, 2, 1]
+    weights2 = [20, 8, 15, 10, 7, 5, 3]
+    amount_calc = random_weighted_range(ranges, weights2)
+    return {
+        "amount": lambda: -(amount_calc())
+        if random.random() < credit_ratio
+        else amount_calc(),
+        "initiated_at": compose(
+            methodcaller("timestamp"), fake.date_time_this_year
+        ),
+        "original_detail": partial(fake.sentence, nb_words=7),
+        "transaction_reference": compose(
+            str.upper, partial(fake.lexify, "?" * 10)
+        ),
+    }
+
+
+def eval_generators(attrs):
+    obj = {}
+    for k, v in attrs.items():
+        if callable(v):
+            obj[k] = v()
+        elif isinstance(v, str) and v in attrs:
+            obj[k] = obj[v]
+        else:
+            obj[k] = v
+    return obj
+
+
+def gen_fake(fake, cls, **kwargs):
+    fn_name = f"fake_{str.lower(cls.__name__)}"
+    if fn := getattr(sys.modules[__name__], fn_name, None):
+        attrs = fn(fake)
+        obj = eval_generators(attrs)
+        res = cls(**obj, **kwargs)
+        return res
+
+
+def gen_fake_entry(
+    session: Session, transaction_count=200, same_account_ratio=0.3
+):
+    # TODO: add items to session and save to test database
+    fake = Faker()
+    Faker.seed(0)
+    user = gen_fake(fake, UserAccount)
+    provider = gen_fake(fake, Provider)
+    account_types = [
+        (MobileMoneyAccount, 0.5),
+        (SafaricomBuygoodsAccount, 0.4),
+        (SafaricomPaybillAccount, 0.1),
+    ]
+    account_pool = list(
+        concat(
+            [
+                [
+                    gen_fake(fake, acc, provider=provider)
+                    for _ in range(ceil(transaction_count * perc))
+                ]
+                for (acc, perc) in account_types
+            ]
+        )
+    )
+    tacc = gen_fake(fake, MobileMoneyAccount, provider=provider, holder=user)
+    picked = []
+    for _ in range(transaction_count):
+        if random.random() < same_account_ratio:
+            pool = picked if picked else account_pool
+            other = random.choice(pool)
+        else:
+            pool = account_pool
+            other = random.choice(pool)
+            if other not in picked:
+                picked.append(other)
+        tx = gen_fake(
+            fake, Transaction, owner_account=tacc, participant_account=other
+        )
